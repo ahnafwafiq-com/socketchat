045a651af0bb6c6442266272d639c1b6d52072ee
ahnafwafiq <hello@ahnafwafiq.com>
2023-07-19 00:42:45 +0600

    Ready for initial deployment

--- app/src/main.ts
+++ app/src/main.ts
@@ -28,7 +28,7 @@ document.addEventListener("keyup", (e: KeyboardEvent) => {
 });
 
 // Socket IO Connection
-const socket = io("http://localhost:8080");
+const socket = io("https://api.socketio.ahnafwafiq.com");
 socket.on("connect", () => {
     if (status) {
         status.innerText = `Connected with server as id "${socket.id}"`;
